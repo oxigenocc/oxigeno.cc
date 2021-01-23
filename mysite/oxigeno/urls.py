a16d24fa67934811bce0836cd722def5db69567e
klaymond <uri.goldberg06@gmail.com>
2021-01-23 00:42:17 -0600

    Main page setup and api

--- mysite/oxigeno/urls.py
+++ mysite/oxigeno/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('/data', views.rest_get, name='rest_get'),
 ]
--- mysite/oxigeno/views.py
+++ mysite/oxigeno/views.py
@@ -1,9 +1,45 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.template import loader
+
+from .models import Distribuidor, Tanque, Concentrador
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the oxigeno index.")
+    template = loader.get_template('oxigeno/index.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
 
 def rest_get(request):
-    return HttpResponse("Hello, world. You're at the oxigeno get api.")
+    distribuidores = Distribuidor.objects.all()
+    resp = []
+    for d in distribuidores:
+        tanques = [
+            {
+                'disponibilidad_renta': x.disponibilidad_renta,
+                'disponibilidad_venta': x.disponibilidad_venta,
+                'disponibilidad_recarga': x.disponibilidad_recarga
+            } for x in d.tanque_set.all()
+        ]
+        concentradores = [
+            {
+                'disponibilidad_renta': x.disponibilidad_renta,
+                'disponibilidad_venta': x.disponibilidad_venta
+            } for x in d.concentrador_set.all()
+        ]
+        data = {
+            'nombre_distribuidor': d.nombre_distribuidor,
+            'horario': d.horario,
+            'estado': d.estado,
+            'direccion': d.direccion,
+            'ciudad': d.ciudad,
+            'a_domicilio': d.a_domicilio,
+            'pago_con_tarjeta': d.pago_con_tarjeta,
+            'notas': d.notas,
+            'telefono': d.telefono,
+            'ultima_actualizacion': d.ultima_actualizacion,
+            'concentradores': concentradores,
+            'tanques': tanques
+        }
+        resp.append(data)
+    return JsonResponse(resp)
