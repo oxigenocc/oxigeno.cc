67821fc4240e1212b4cd5b901289898c2c095724
klaymond <uri.goldberg06@gmail.com>
2021-01-22 21:53:23 -0600

    Secret key removed

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'z))*(0-cx=dxsx2@^3h@3lo-29uq03k^rm&31^o6z&xl%t=sp)'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
