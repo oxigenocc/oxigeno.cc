6157741ed1bea658037d187536d068d8c601b64c
klaymond <uri.goldberg06@gmail.com>
2021-01-28 21:48:58 -0600

    history fix

--- mysite/oxigeno/admin.py
+++ mysite/oxigeno/admin.py
@@ -3,6 +3,7 @@ import json
 from django.contrib import admin
 from django_google_maps import widgets as map_widgets
 from django_google_maps import fields as map_fields
+from simple_history.admin import SimpleHistoryAdmin
 
 # Register your models here.
 from .models import Distribuidor, Tanque, Concentrador
@@ -19,6 +20,6 @@ class DistribuidorAdmin(admin.ModelAdmin):
       })},
     }
 
-admin.site.register(Distribuidor, DistribuidorAdmin)
-admin.site.register(Tanque)
-admin.site.register(Concentrador)
+admin.site.register(Distribuidor, DistribuidorAdmin, SimpleHistoryAdmin)
+admin.site.register(Tanque, SimpleHistoryAdmin)
+admin.site.register(Concentrador, SimpleHistoryAdmin)
