577dacca8eaab44ebf0f1388e704dc675098f517
klaymond <uri.goldberg06@gmail.com>
2021-02-03 23:14:37 -0600

    manager and new api

--- mysite/oxigeno/urls.py
+++ mysite/oxigeno/urls.py
@@ -3,8 +3,11 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 from . import views
+from . import views_v1_1
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
     path('data', views.rest_get, name='rest_get'),
+    path('v1.1/data', views_v1_1.rest_get, name='rest_get_v1_1'),
+    path('v1.1/data/<int:id_distribuidor>', views_v1_1.rest_get_single, name='rest_get_single_v1_1'),
 ]
