1bf7a38f88fecfafcb9739d45c919b83cf13001d
klaymond <uri.goldberg06@gmail.com>
2021-02-10 22:55:24 -0600

    lowered import level

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from ..oxigeno.views_v1_2 import post_distribuidor_potencial
+from .oxigeno.views_v1_2 import post_distribuidor_potencial
 
 urlpatterns = [
     path('admin/', admin.site.urls),
