55e0c853e357e3611b687e6a87761f64cb4bfc4d
klaymond <uri.goldberg06@gmail.com>
2021-02-24 17:12:07 -0600

    reverted filters

--- mysite/oxigeno/api/v2/filters.py
+++ mysite/oxigeno/api/v2/filters.py
@@ -28,12 +28,12 @@ class DistribuidorFilterSet(FilterSet):
     incluirBajas = NumberFilter(
         method='filter_field_bool',
         field_name='dar_de_baja')
-    # abiertoSabado = NumberFilter(
-    #     method='filter_field_bool',
-    #     field_name='abre_sabado')
-    # abiertoDomingo = NumberFilter(
-    #     method='filter_field_bool',
-    #     field_name='abre_domingo')
+    abiertoSabado = NumberFilter(
+        method='filter_field_bool',
+        field_name='abre_sabado')
+    abiertoDomingo = NumberFilter(
+        method='filter_field_bool',
+        field_name='abre_domingo')
     abierto24 = NumberFilter(
         method='filter_field_bool',
         field_name='abre_24h')
@@ -62,4 +62,4 @@ class DistribuidorFilterSet(FilterSet):
         fields = ('tanqueVenta', 'tanqueRecarga', 'tanqueRenta',
                   'concentradorVenta', 'concentradorRenta', 'pagoConTarjeta',
                   'aDomicilio', 'incluirBajas', 'nombreComo',
-                  'abierto24', 'gratis',) #'abreSabado', 'abreDomingo')
+                  'abierto24', 'gratis', 'abreSabado', 'abreDomingo')
