4c7c851c04a2c4773070ef1e56439cb3fae02ecc
klaymond <uri.goldberg06@gmail.com>
2021-02-25 23:00:52 -0600

    urls fixed forever

--- dev-deploy.sh
+++ dev-deploy.sh
@@ -1,4 +1,11 @@
-cd ..; rm -rf static
-source venv/bin/activate; cd mysite; python manage.py collectstatic
-cd ..; tar -czvf static/static.tar.gz static
+#!/bin/bash
+cd ..
+rm -rf static
+cd oxigeno.cc
+source venv/bin/activate
+cd mysite
+python manage.py collectstatic
+cd ../..
+tar -czvf static/static.tar.gz static
+cd oxigeno.cc
 docker-compose up --build
