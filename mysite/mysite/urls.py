efe3b085d873f4221769a25e7c01b654ab1c5fc1
klaymond <uri.goldberg06@gmail.com>
2021-02-25 23:44:26 -0600

    urls fix

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from api.v2 import urls as APIV1Urls
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
